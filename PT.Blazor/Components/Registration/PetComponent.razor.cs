61c7af5b7686bdb4d003c3be0214ae00a4b9e6ed
Siarhei Samusik <Siarhei.Samusik@ventionteams.com>
2026-03-24 02:22:12 +0300

    Improved pet component

--- PT.Blazor/Components/Registration/PetComponent.razor.cs
+++ PT.Blazor/Components/Registration/PetComponent.razor.cs
@@ -1,9 +1,45 @@
-namespace PT.Blazor.Components.Registration
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace PT.Blazor.Components.Registration;
+
+public partial class PetComponent
 {
-    public partial class PetComponent
+    private string petname = null!;
+    private string city = null!;
+
+    private IBrowserFile? uploadedAvatar;
+
+    private string avatarPreview = string.Empty;
+
+    protected override void OnInitialized()
     {
-        private string petname = null!;
-        private string city = null!;
-        private string street = null!;
+        ResetPicture();
+    }
+
+    private void ResetPicture()
+    {
+        avatarPreview = string.Empty;
+        StateHasChanged();
+    }
+
+    private async Task SwapPicture()
+    {
+        if (uploadedAvatar is null)
+        {
+            return;
+        }
+        if (uploadedAvatar.Size > 5242880)
+        {
+            return;
+        }
+        StateHasChanged();
+
+        using var stream = uploadedAvatar.OpenReadStream(5242880);
+        using var memoryStream = new MemoryStream();
+        await stream.CopyToAsync(memoryStream);
+        var fileBytes = memoryStream.ToArray();
+
+        avatarPreview = $"data:{uploadedAvatar.ContentType};base64,{Convert.ToBase64String(fileBytes)}";
+        uploadedAvatar = null;
     }
 }
