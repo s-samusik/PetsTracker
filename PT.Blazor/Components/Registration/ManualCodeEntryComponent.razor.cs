1030d347562fdba196003097a11e47d4a2d8b09d
Siarhei Samusik <Siarhei.Samusik@ventionteams.com>
2026-03-23 23:59:21 +0300

    Fixed manuall input

--- PT.Blazor/Components/Registration/ManualCodeEntryComponent.razor.cs
+++ PT.Blazor/Components/Registration/ManualCodeEntryComponent.razor.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Components;
+using MudBlazor;
 
 namespace PT.Blazor.Components.Registration;
 
@@ -8,15 +9,22 @@ public partial class ManualCodeEntryComponent
 
     [Inject] private NavigationManager Nav { get; set; } = default!;
 
+    public PatternMask QrCodeMask = new("AA-11-AAA")
+    {
+        MaskChars = [new MaskChar('A', @"[a-zA-Z]"), new MaskChar('1', @"[1-9]")],
+        Placeholder = ' ',
+        CleanDelimiters = true,
+        Transformation = AllUpperCase
+    };
+
     private string manualCode = null!;
 
-    void GoToRegistration()
-    {
-        Nav.NavigateTo($"/card/{manualCode}", forceLoad: true);
-    }
+    private void GoToRegistration()
+        => Nav.NavigateTo($"/card/{manualCode}", forceLoad: true);
 
-    void GoHome()
-    {
-        Nav.NavigateTo("/", forceLoad: true);
-    }
+    private void GoHome()
+        => Nav.NavigateTo("/", forceLoad: true);
+
+    private static char AllUpperCase(char c) 
+        => c.ToString().ToUpperInvariant()[0];
 }
