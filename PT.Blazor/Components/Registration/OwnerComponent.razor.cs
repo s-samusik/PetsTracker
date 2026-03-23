8666a00cbde95de50fd546263f91ecd9fdea17bf
Siarhei Samusik <Siarhei.Samusik@ventionteams.com>
2026-03-24 01:23:33 +0300

    Improved owner component

--- PT.Blazor/Components/Registration/OwnerComponent.razor.cs
+++ PT.Blazor/Components/Registration/OwnerComponent.razor.cs
@@ -1,8 +1,34 @@
+using MudBlazor;
+
 namespace PT.Blazor.Components.Registration;
 
 public partial class OwnerComponent
 {
     private string phoneNumber = null!;
-    private string username = null!;
     private bool isDisabled;
+
+    private List<string> SelectedSocials { get; set; } = [];
+    private Dictionary<string, string> SocialNicks { get; set; } = [];
+
+    private void OnSelectedSocialsChanged(IEnumerable<string> values)
+    {
+        SelectedSocials = [.. values];
+
+        foreach (var v in SelectedSocials)
+        {
+            if (!SocialNicks.ContainsKey(v))
+                SocialNicks[v] = string.Empty;
+        }
+    }
+
+    private static string GetSocialIcon(string social)
+    {
+        return social switch
+        {
+            "Instagram" => Icons.Custom.Brands.Instagram,
+            "Telegram" => Icons.Custom.Brands.Telegram,
+            "TikTok" => Icons.Custom.Brands.TikTok,
+            _ => Icons.Material.Filled.Person
+        };
+    }
 }
